578046ba9ff77d103c94c6b106b368d3a5ef5f11
GajananDhangude <dhangudegajanan27@gmail.com>
2025-11-22 11:15:50 +0530

    Added libraries to Train model

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -1,5 +1,12 @@
-import pandas as pd
-import numpy as np
 import os
 import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import AdaBoostClassifier , GradientBoostingRegressor , RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score , mean_absolute_error , mean_squared_error
+
+from src.exception import CustomException
+from src.logger import logging
